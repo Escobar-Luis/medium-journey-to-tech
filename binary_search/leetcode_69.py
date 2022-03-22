cc635b2321837d3fd3a4ce04794ab287745f67c5
Luis Escobar <lae311@stern.nyu.edu>
2022-03-22 08:14:05 -0400

    leetcode 69

---
+++ binary_search/leetcode_69.py
@@ -0,0 +1,28 @@
+link_to_problem = 'https://leetcode.com/problems/sqrtx/'
+
+# The problem is equivalent to finding the boundary of elements < n and elements >= n.
+# If we apply a filter of the value * value = n (A.K.A return boolean where value squared = n) then the problem is reduced to finding the last true element in a boolean array.
+
+
+def square_root(n: int) -> int:
+    # We handle edge case of the integer being equal to 0
+    if n == 0:
+        return 0
+    # construct left and right pointers where the right pointer is equal to the given number
+    left, right = 1, n
+    # construct of boundary index
+    res = -1
+    # Make sure the pointers never cross each other by using a equality operator
+    while left <= right:
+        # At start of every iteration, re-construct midpoint
+        mid = (left + right) // 2
+        # If midpoint squared id less than or equal to our given number
+        if mid * mid <= n:
+            # update boundary index to mid and move right pointer
+            res = mid
+            left = mid + 1
+        # else if mid is larger than midpoint
+        else:
+            # Move the right pointer
+            right = mid - 1
+    return res
--- binary_search/leetcode_744.py
+++ binary_search/leetcode_744.py
@@ -7,13 +7,23 @@ link_to_problem = 'https://leetcode.com/problems/find-smallest-letter-greater-th
 from typing import List
 
 def first_not_smaller(arr: List[int], target: int) -> int:
+    # Initiate left and right pointers, where right pointer is set to the length of the given array - 1 because python reads list starting at 0 not 1 so we subtract 1 from our right pointer to make sure that it is in bounds.
     left, right = 0, len(arr) - 1
+    # Track of the leftmost index of the element that is larger than or equal to target (a.k.a makes this statement true). Also, since we are guaranteed to find a satisfying number per question constraints, the intitating number can be anything since it will be updated in the future.
     boundary_index = -1
+    # Note the equality to make sure either pointer never cross to each others respective domains
     while left <= right:
+        # recalculate mid in every iteration by using the new values of left and right pointers
         mid = (left + right) // 2
+        # if mid of our array's value is greater than or equal to the given target
         if arr[mid] >= target:
+            # we set the boundary index to our mid since we have to return the index of the element that satisfies condition
             boundary_index = mid
+            # move right pointer to mid and subtract one from it to disregard the mid element as well
             right = mid - 1
+        # the mid value is less than or equal to the given target
         else:
+            # move left pointer to mid and ADD one to it to disregard the mid element as well
             left = mid + 1
+    # return boundary index after sorting whole array
     return boundary_index
