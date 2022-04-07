5a525790e08c3cc977030794def7b2a36efa10d4
Luis Escobar <lae311@stern.nyu.edu>
2022-04-07 14:28:34 -0400

    dfs

--- binary_search/uglyNum3.py
+++ binary_search/uglyNum3.py
@@ -1,10 +1,12 @@
 from math import gcd
 
 def nth_ugly_number(n: int, a: int, b: int, c: int) -> int:
+    # variables needed to calculate total area of venn diagram i.e count of ugly numbers
     ab = a * b // gcd(a, b)
     bc = b * c // gcd(b, c)
     ac = a * c // gcd(a, c)
     abc = ab * c // gcd(ab, c)
+    # Find the # of integers that are divisible by the three numbers (a,b,c) i.e ugly numbers
     def ugly_number_count(k: int) -> int:
         return (k // a + k // b + k // c) - (k // ab + k // bc + k // ac) + k // abc
     min_ptr = n
@@ -17,4 +19,5 @@ def nth_ugly_number(n: int, a: int, b: int, c: int) -> int:
         else:
             boundaryIndex = midpoint
             max_ptr = midpoint - 1
+            # to prevent integer overflows we take index and modulo it by the guven
     return boundaryIndex % (10 ** 9 + 7)
---
+++ dfs/maxDepthOfTree.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def tree_max_depth(root: Node) -> int:
+    def dfs(root): # we don't actually need an inner function doing it here just keep consistent with other solutions
+        # null node adds no depth
+        if not root:
+            return 0
+        # depth of current node's subtree = max depth of the two subtrees + 1 provided by current node
+        return max(dfs(root.left), dfs(root.right)) + 1
+    return dfs(root)
+
+def build_tree(nodes, f):
+    val = next(nodes)
+    if val == 'x': return None
+    left = build_tree(nodes, f)
+    right = build_tree(nodes, f)
+    return Node(f(val), left, right)
