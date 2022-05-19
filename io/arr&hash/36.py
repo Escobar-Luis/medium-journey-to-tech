65a1d9f9caa2fdea60465eaee507614dcf84c040
Luis Escobar <lae311@stern.nyu.edu>
2022-05-19 11:47:06 -0400

    sodoku annon

--- io/arr&hash/36.py
+++ io/arr&hash/36.py
@@ -1,11 +1,12 @@
 from typing import List
 import collections
 
-def isValidSudoku(self, board: List[List[str]]) -> bool:
+def isValidSudoku(board: List[List[str]]) -> bool:
+    # Used to keep track of which elements we have seen
     cols = collections.defaultdict(set)
     rows = collections.defaultdict(set)
     squares = collections.defaultdict(set) # key = (r /3, c /3)
-    
+    # loop through entire board
     for r in range(9):
         for c in range(9):
             if board[r][c] == ".":
