a1f59499549cd3acc32830db8a1adf4c15a6dd17
Luis Escobar <lae311@stern.nyu.edu>
2022-05-20 11:58:34 -0400

    redos

--- io/arr&hash/49.py
+++ io/arr&hash/49.py
@@ -14,12 +14,9 @@ def groupAnagrams(strs: List[str]) -> List[List[str]]:
             # every pass for each character in a word, we figure out each character unicode value and iterate its count by one then we access or create key in hash using tuple count and append the current interations word in it. 
             for c in s:
                 # ord method converts a character to its unicode value so we can track which character is alphanumerical or a special character
-                first = ord(c)
-                sec=ord('a')
                 # the index can be given by subtracting the unicode values
                 diff=ord(c) - ord('a')
                 # acess the index of the specific character
-                h=count[diff]
                 count[diff] += 1
             # we change count list to a tuple because they are immutable yet list are which is a no no for python dicts
             ans[tuple(count)].append(s)
