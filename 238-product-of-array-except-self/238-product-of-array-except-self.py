ad4b4c517eec5933a3d3d7b973b194fcbc11cd0d
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-05-31 14:24:24 -0400

    Time: 235 ms (92.35%), Space: 21.1 MB (81.07%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,14 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-#   result array with 1s up to length of nums array, I first calculate the prefixes at every position and multiply to my results arr, then calculate the postfixes and multiply them to my results array and then return result
+#         At every position I want to calculate my prefix and postfix and multiply those together
+#         res = [1] for i in range(len(nums))
+#         prefix =1
+#         first pass, we update our res[i] to prefix, and then update prefix to product of current value to nums[i]
+#         postfix=1
+#         second pass backwards, we update our res[i] not to postfix but the product of the indexes prefix at res[i] with the postfix, and then update postfix to product of itself with nums[i]
+# return res
         res = [1] * len(nums)
-        prefix=1
-#         set res[i] to prefix and update it to the new value
+        prefix =1
         for i in range(len(nums)):
             res[i] = prefix
             prefix *= nums[i]
-        postfix=1
+        postfix =1
         for i in range(len(nums)-1,-1,-1):
-            res[i]*=postfix
-            postfix*=nums[i]
+            res[i] *= postfix
+            postfix *= nums[i]
         return res
