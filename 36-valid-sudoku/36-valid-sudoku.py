ec8391fa20ac78ebe2bb93ac823f64688429016c
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-01 12:26:00 -0400

    Time: 94 ms (94.84%), Space: 13.9 MB (34.46%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,11 +1,5 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-#         I need acess every value in the board
-#         I need to track repetitive digits
-#         Also, in a 3x 3 box
-#         2 hashes where key is the the row, and the value is a set and we ask if the value at every position is in our hashset
-# hash where the key (r //3, c//3) and value is a set
-
         rows = collections.defaultdict(set)
         cols = collections.defaultdict(set)
         squares = collections.defaultdict(set)
@@ -13,12 +7,11 @@ class Solution:
             for c in range(9):
                 if board[r][c] == '.' or board[r][c] == ',':
                     continue
-                elif (board[r][c] in rows[r] or
-                  board[r][c] in cols[c] or
-                  board[r][c] in squares[(r//3,c//3)]):
+                if (board[r][c] in rows[r] or
+                   board[r][c] in cols[c] or
+                   board[r][c] in squares[(r//3,c//3)]):
                     return False
-                else:
-                    rows[r].add(board[r][c])
-                    cols[c].add(board[r][c])
-                    squares[(r//3,c//3)].add(board[r][c])
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
         return True
