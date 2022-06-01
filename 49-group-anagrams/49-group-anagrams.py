c085755e1432db629035af68b7f94bfd2e0c2b93
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-01 10:34:16 -0400

    Time: 127 ms (62.56%), Space: 17.9 MB (55.02%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,25 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         1: Hash, pass through arr, sort every word, save the sorted word as key in hash and append original, return values
-        # h = {}
-        # for s in strs:
-        #     sort = ''.join(sorted(s))
-        #     if sort in h:
-        #         h[sort].append(s)
-        #     else:
-        #         h[sort] = [s]
-        # return h.values()
-        
-        # 2: hash, pass through arr, create arr containing 26 0s, for every letter in word subtract ordinal(a) from it and use it to iterate index count, then save count to hash and append word to it
-        h = {}
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                pos = ord('a') - ord(c)
-                count[pos] +=1
-            x=tuple(count)
-            if x in h:
-                h[x].append(s)
-            else:
-                h[x] = [s]
+#         Sorting the word or Matching the counts
+#         O(n log n), hash, every pass we sort the word and ask if that sorted word is in our hash, if it is we append the original word and if not we create it, at the end we return all the values of the hash
+#         O(m*n), hash, every pass we create arr of 26 0s which is our key, then we loop through every word in the str, calulate its index in our arr by subtracting the ord(a)- ord(c), and we iterate the count at the index in our array, then outside that nested loop we set the modified arr containing our counts as a key in our hash and append the word that made that key
+        h = collections.defaultdict(list)
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            h[sorted_word].append(word)
         return h.values()
