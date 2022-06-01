18f1c8655456d11c1c28422a6791d4469271e347
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-01 12:20:19 -0400

    Time: 116 ms (73.39%), Space: 19.6 MB (11.27%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,17 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-#         track counts in hash, iterate through hash and get max, append it to res and delete key, and repeat until len(res) == k O(n^2)
-#         track counts in hash, freq arr where the length == len(nums) and the index is frequency and the value at that index is gonna be a list,
+        # Bucket Sort
         h = {}
+        freq= [[] for i in range(len(nums)+1)]
         for n in nums:
-            h[n] = 1 + h.get(n,0)
-        freq = [[] for i in range(len(nums)+1)]
+            h[n] = 1+ h.get(n,0)
+        res = []
         for n,c in h.items():
             freq[c].append(n)
-        res = []
-        for i in range(len(freq) -1,-1,-1):
-            for word in freq[i]:
-                res.append(word)
-                if len(res)==k:
+        for i in range(len(freq)-1,-1,-1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
                     return res
-        
+            
