d313d91a1d35bd288ad293dbec1c2221e082fe9a
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-01 10:36:50 -0400

    Time: 179 ms (25.26%), Space: 19.9 MB (21.00%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -3,8 +3,16 @@ class Solution:
 #         Sorting the word or Matching the counts
 #         O(n log n), hash, every pass we sort the word and ask if that sorted word is in our hash, if it is we append the original word and if not we create it, at the end we return all the values of the hash
 #         O(m*n), hash, every pass we create arr of 26 0s which is our key, then we loop through every word in the str, calulate its index in our arr by subtracting the ord(a)- ord(c), and we iterate the count at the index in our array, then outside that nested loop we set the modified arr containing our counts as a key in our hash and append the word that made that key
+        # h = collections.defaultdict(list)
+        # for word in strs:
+        #     sorted_word = ''.join(sorted(word))
+        #     h[sorted_word].append(word)
+        # return h.values()
         h = collections.defaultdict(list)
         for word in strs:
-            sorted_word = ''.join(sorted(word))
-            h[sorted_word].append(word)
+            key = [0] * 26
+            for c in word:
+                ind = ord('a')-ord(c)
+                key[ind] +=1
+            h[tuple(key)].append(word)
         return h.values()
