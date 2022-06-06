39d7ae60d3ae44d4a31d5324844bc1adb9c47e8c
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-06 09:31:31 -0400

    Time: 4703 ms (5.00%), Space: 28.5 MB (30.46%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -3,9 +3,10 @@ class Solution:
         s = set(nums)
         res = 0
         for n in nums:
-            if (n-1) not in s:
+            if n-1 not in s:
                 l = 1
                 while n+l in s:
                     l+=1
                 res = max(res,l)
         return res
+                
