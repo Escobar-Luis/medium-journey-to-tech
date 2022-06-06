d50f2783cc28bdc9c26584d564d6735b25db7d43
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-06 09:13:51 -0400

    Time: 173 ms (29.33%), Space: 19.8 MB (21.05%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,11 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # O (m*n)
         h = collections.defaultdict(list)
-        for word in strs:
+        for s in strs:
             key = [0]*26
-            for c in word:
-                ind = ord('a')-ord(c)
-                key[ind] +=1
-            h[tuple(key)].append(word)
+            for c in s:
+                ind = ord('a')- ord(c)
+                key[ind] += 1
+            h[tuple(key)].append(s)
         return h.values()
