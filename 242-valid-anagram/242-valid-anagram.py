d48c7b4008c16bba140a3601db919774bcae95a6
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-14 08:51:15 -0400

    Time: 49 ms (89.44%), Space: 14.7 MB (26.45%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,15 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        count1= {}
-        count2={}
-        for i in range(len(s)):
-            count1[s[i]] = 1 + count1.get(s[i],0)
-            count2[t[i]] = 1 + count2.get(t[i],0)
-        for c in s:
-            if c not in count2 or count1[c]!=count2[c]:
-                return False
-        return True
-            
+        s=''.join(sorted(s))
+        t=''.join(sorted(t))
+        if s == t:
+            return True
+        else: return False
         
