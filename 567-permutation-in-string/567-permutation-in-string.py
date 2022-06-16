a727212cc285cfe5830250a0d680cfd09e26454e
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-16 17:54:05 -0400

    Time: 72 ms (68.91%), Space: 14 MB (18.56%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -5,37 +5,34 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        if len(s1)> len(s2): return False
-        #count arr
-        h1,h2 = [0]*26, [0]*26
+        if len(s1) > len(s2): return False
         
+        s1Count, s2Count = [0]*26, [0]*26
         for i in range(len(s1)):
-            h1[ord(s1[i]) - ord('a')] +=1
-            h2[ord(s2[i]) - ord('a')] +=1
-            
+            s1Count[ord(s1[i])-ord('a')] += 1
+            s2Count[ord(s2[i])-ord('a')] += 1
+        
         matches = 0
         for i in range(26):
-            matches += 1 if h1[i]==h2[i] else 0
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
         
-        l = 0
+        l =0
         for r in range(len(s1), len(s2)):
-            if matches == 26: return True
-            
-            index = ord(s2[r]) - ord('a')
-            h2[index] +=1
-            if h2[index] == h1[index]:
+            if matches == 26:
+                return True
+            ind = ord(s2[r]) - ord('a')
+            s2Count[ind] +=1
+            if s1Count[ind] == s2Count[ind]:
                 matches +=1
-            elif h2[index] == h1[index]+1:
+            elif s1Count[ind] +1 == s2Count[ind]:
                 matches -=1
             
-            index = ord(s2[l]) - ord('a')
-            h2[index] -=1
-            if h2[index] == h1[index]:
+            ind = ord(s2[l]) - ord('a')
+            s2Count[ind] -=1
+            if s1Count[ind] == s2Count[ind]:
                 matches +=1
-            elif h2[index] == h1[index]-1:
+            elif s1Count[ind] -1 == s2Count[ind]:
                 matches -=1
             l+=1
-            
         return matches == 26
-        
-        
+            
