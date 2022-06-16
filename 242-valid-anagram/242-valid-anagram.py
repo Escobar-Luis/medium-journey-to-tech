507d70a957c2852299fc7bc84fddb08da2d4c75c
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-16 16:08:02 -0400

    Time: 103 ms (13.80%), Space: 14.5 MB (34.42%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -2,7 +2,17 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!= len(t):
             return False
-        o = ''.join(sorted(s))
-        d = ''.join(sorted(t))
-        return o == d
+        # o = ''.join(sorted(s))
+        # d = ''.join(sorted(t))
+        # return o == d
+        ho={}
+        hd={}
+        for i in range(len(s)):
+            ho[s[i]]= 1 + ho.get(s[i],0)
+            hd[t[i]]= 1 + hd.get(t[i],0)
+        
+        for n in s:
+            if n not in hd or ho[n] != hd[n]:
+                return False
+        return True
         
