1b23402071a852106400c97db833cad133fdd96a
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-17 09:08:39 -0400

    Time: 63 ms (77.75%), Space: 13.9 MB (36.36%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -5,34 +5,40 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        if len(s1) > len(s2): return False
+        if len(s1)>len(s2): return False
         
-        s1Count, s2Count = [0]*26, [0]*26
+        oc = [0] * 26
+        dc = [0] * 26
+        
+#       populating first counts
         for i in range(len(s1)):
-            s1Count[ord(s1[i])-ord('a')] += 1
-            s2Count[ord(s2[i])-ord('a')] += 1
-        
+            oc[ord(s1[i]) - ord('a')] +=1
+            dc[ord(s2[i]) - ord('a')] +=1
+            
         matches = 0
         for i in range(26):
-            matches += (1 if s1Count[i] == s2Count[i] else 0)
-        
-        l =0
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
-                return True
-            ind = ord(s2[r]) - ord('a')
-            s2Count[ind] +=1
-            if s1Count[ind] == s2Count[ind]:
-                matches +=1
-            elif s1Count[ind] +1 == s2Count[ind]:
-                matches -=1
+            if oc[i] == dc[i]:
+                matches += 1
+            else:
+                continue
             
-            ind = ord(s2[l]) - ord('a')
-            s2Count[ind] -=1
-            if s1Count[ind] == s2Count[ind]:
+        
+        l = 0
+        for r in range(len(s1),len(s2)):
+            if matches == 26: return True
+            
+            ind = ord(s2[r]) - ord('a')
+            dc[ind] +=1
+            if oc[ind] == dc[ind]:
                 matches +=1
-            elif s1Count[ind] -1 == s2Count[ind]:
+            elif oc[ind] +1 == dc[ind]:
+                matches -=1
+                
+            ind = ord(s2[l]) - ord('a')
+            dc[ind] -=1
+            if oc[ind] == dc[ind]:
+                matches +=1
+            elif oc[ind] -1 == dc[ind]:
                 matches -=1
             l+=1
         return matches == 26
-            
