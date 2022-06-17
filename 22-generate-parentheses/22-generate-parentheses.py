39822f766b99b51db541330a9441d0cc4f8127eb
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-17 09:50:52 -0400

    Time: 56 ms (40.08%), Space: 14.2 MB (39.06%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -3,17 +3,17 @@ class Solution:
         s = []
         res = []
         
-        def b(o,c):
-            if o ==c==n:
+        def recurse(o,c):
+            if o == c == n:
                 res.append(''.join(s))
                 return
-            if o<n:
+            if o < n:
                 s.append('(')
-                b(o+1,c)
+                recurse(o+1, c)
                 s.pop()
-            if c<o:
+            if c < o:
                 s.append(')')
-                b(o,c+1)
+                recurse(o, c+1)
                 s.pop()
-        b(0,0)
-        return res            
+        recurse(0,0)
+        return res
