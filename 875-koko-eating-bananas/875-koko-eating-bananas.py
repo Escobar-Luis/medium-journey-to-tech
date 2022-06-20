942b68dd91700c1f4821eaf587c37862c2dbb3ac
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-20 10:46:30 -0400

    Time: 981 ms (10.30%), Space: 15.3 MB (99.26%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -1,18 +1,17 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l,r = 1, max(piles)
+        
         k = 0
         
         while l<=r:
-            m = (l+r)//2
-            
-            time =0
+            m = (l+r) //2
+            hours = 0
             for p in piles:
-#                 round up
-                time += math.ceil(p/m)
-            if time <= h:
+                hours += math.ceil(p/m)
+            if hours > h:
+                l = m +1
+            else:
                 k = m
                 r = m-1
-            else:
-                l = m+1
         return k
