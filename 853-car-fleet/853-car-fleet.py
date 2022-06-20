8be5aa8a6b95d338faa88fcd514357362e6097b2
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-20 10:12:28 -0400

    Time: 1579 ms (29.27%), Space: 33.8 MB (69.62%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -3,10 +3,9 @@ class Solution:
         pairs = [[p,s] for p,s in zip(position, speed)]
         
         stack = []
-        for p,s in sorted(pairs)[::-1]:
-#         appending time to stack
-            stack.append((target-p)/s)
-#           if we have more than 2 cars in stack and if the time we just appended is less than the previous time than we know they will collide and form a fleet so we pop from our stack
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+        for p,s in reversed(sorted(pairs)):
+            time = ((target - p)/ s)
+            stack.append(time)
+            if len(stack)>=2 and stack [-2] >= stack[-1]:
                 stack.pop()
         return len(stack)
