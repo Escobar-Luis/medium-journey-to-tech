e0e196cbd84c59198e116b791eba313eab602138
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-20 09:56:48 -0400

    Time: 52 ms (51.44%), Space: 14.2 MB (39.40%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,19 +1,20 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        s = []
         res = []
+        stack = []
         
         def recurse(o,c):
             if o == c == n:
-                res.append(''.join(s))
+                res.append(''.join(stack))
                 return
-            if o < n:
-                s.append('(')
-                recurse(o+1, c)
-                s.pop()
-            if c < o:
-                s.append(')')
-                recurse(o, c+1)
-                s.pop()
+            if o< n:
+                stack.append('(')
+                recurse(o+1,c)
+                stack.pop()
+            if c<o:
+                stack.append(')')
+                recurse(o,c+1)
+                stack.pop()
+        
         recurse(0,0)
         return res
