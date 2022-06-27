79351d0107aa39ac556a4955940a2af857a265b8
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-27 12:31:01 -0400

    Time: 1818 ms (40.07%), Space: 24.8 MB (70.77%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,11 +1,10 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0]* len(temperatures)
         s = []
-        res = [0] * len(temperatures)
-        
-        for i, v in enumerate(temperatures):
+        for i,v in enumerate(temperatures):
             while s and v > s[-1][1]:
                 si,sv = s.pop()
-                res[si] = i - si
+                res[si] = i-si
             s.append([i,v])
         return res
