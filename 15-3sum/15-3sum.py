8c5c7354237ee02521a3c3043b6d1a86add5af4e
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-27 11:46:51 -0400

    Time: 940 ms (49.10%), Space: 16.6 MB (88.08%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -6,15 +6,16 @@ class Solution(object):
         """
         res = []
         nums.sort()
-        for i, v in enumerate(nums):
+        
+        for i,v in enumerate(nums):
             if i>0 and v == nums[i-1]:
                 continue
             l,r = i+1, len(nums)-1
             while l<r:
-                s = v + nums[l]+ nums[r]
-                if s> 0:
+                s = v + nums[l] + nums[r]
+                if s > 0:
                     r-=1
-                elif s<0:
+                elif s <0:
                     l+=1
                 else:
                     res.append([v,nums[l],nums[r]])
