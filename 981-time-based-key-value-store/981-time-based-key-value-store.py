7a07f800578e5d4f829d88f8ca365dda346464e0
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-29 13:04:11 -0400

    Time: 1906 ms (5.02%), Space: 72.4 MB (22.54%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -10,12 +10,12 @@ class TimeMap:
 
     def get(self, key: str, timestamp: int) -> str:
         res, values = "", self.hash.get(key,[])
-        l,r = 0, len(values) - 1
+        l,r = 0, len(values)-1
         while l<=r:
-            m = (l+r) // 2
+            m = (l+r)//2
             if values[m][1] <= timestamp:
                 res = values[m][0]
-                l = m+1
+                l= m+1
             else:
                 r = m-1
         return res
