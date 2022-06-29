b74d1666187b33bb2d1f70cb689a258194c0fd83
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-06-29 12:06:07 -0400

    Time: 99 ms (5.19%), Space: 14.4 MB (19.01%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -4,11 +4,11 @@ class Solution:
         
         while l<=r:
             m = (l+r)//2
-            if nums[m]== target:
+            if nums[m] == target:
                 return m
-            elif nums[m] >= nums[l]:
+            if nums[m] >= nums[l]:
                 if target > nums[m] or target < nums[l]:
-                    l = m+1
+                    l= m+1
                 else:
                     r = m-1
             else:
