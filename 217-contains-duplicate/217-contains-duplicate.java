e1165e85e4f621f7856e702c65b5b310fa909255
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-07-16 10:02:26 -0400

    Time: 9 ms (83.62%), Space: 71.5 MB (19.37%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet seen = new HashSet();
-        for (int i = 0; i < nums.length; i++){
-            if (seen.contains(nums[i])){
+        Set<Integer> set = new HashSet<Integer> ();
+        
+        for (int i = 0; i< nums.length;i++){
+            if (set.contains(nums[i])){
                 return true;
             }
-            seen.add(nums[i]);
-            
+            set.add(nums[i]);
         }
         return false;
     }
