644086bf89d3bd38c7103c75edcb8a58d6a8b14a
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-08-03 09:34:05 -0400

    Time: 78 ms (25.04%), Space: 18.8 MB (13.54%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -10,14 +10,12 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        
         def dfs(root):
             if not root:
                 return [True, 0]
             
             left, right = dfs(root.left), dfs(root.right)
-#             if left and right subtrees are balanced AND if from the root node we are balanced
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <=1
             return [balanced, 1 + max(left[1], right[1])]
         
         return dfs(root)[0]
