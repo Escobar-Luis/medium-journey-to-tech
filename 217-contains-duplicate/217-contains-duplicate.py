1143d62bc4717628edee5c7b209c0fe518da1c0c
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-09-08 10:30:09 -0400

    Time: 961 ms (5.03%), Space: 23.9 MB (63.70%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,8 +1,12 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        s = set()
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = set()
         for n in nums:
-            if n in s:
+            if n in seen:
                 return True
-            s.add(n)
+            seen.add(n)
         return False
