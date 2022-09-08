90a6df8f482f8caa5f67329def03de5755df29ca
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-09-08 10:57:13 -0400

    Time: 76 ms (68.30%), Space: 14.4 MB (44.48%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -6,8 +6,8 @@ class Solution(object):
         :rtype: List[int]
         """
         h = {}
-        for i, n in enumerate(nums):
-            need = target-n
+        for i,n in enumerate(nums):
+            need = target - n
             if need in h:
-                return [h[need], i]
-            h[n]=i
+                return [h[need], i] 
+            h[n] = i
