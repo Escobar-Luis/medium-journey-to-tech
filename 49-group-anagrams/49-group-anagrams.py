5635e366bcd070740076b0262d83e0f063c9ff7b
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-09-08 11:04:01 -0400

    Time: 171 ms (26.85%), Space: 17.6 MB (70.87%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,17 +1,11 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # h=collections.defaultdict(list)
-        # for s in strs:
-        #     freq = [0] * 26
-        #     for c in s:
-        #         ind = ord(c)-ord('a')
-        #         freq[ind] +=1
-        #     h[tuple(freq)].append(s)
-        # return list(h.values())
-        h=collections.defaultdict(list)
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        h = collections.defaultdict(list)
         for s in strs:
-            so = ''.join(sorted(s))
-            h[so].append(s)
-        return list(h.values())
-            
-                
+            sort = ''.join(sorted(s))
+            h[sort].append(s)
+        return h.values()
