5a1a6b3ec7c1507f390a2ee228210171f559d58c
Luis Escobar <76699784+Escobar-Luis@users.noreply.github.com>
2022-09-08 10:47:19 -0400

    Time: 87 ms (27.40%), Space: 15.4 MB (9.86%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -8,9 +8,9 @@ class Solution(object):
         if len(s) != len(t):
             return False
         
-        countS, countT = {},{}
+        countS, countT = {}, {}
         
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
+            countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i],0)
         return countS == countT
